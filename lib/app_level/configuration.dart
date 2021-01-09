f242c660b9950128707e3d2c4eed323ad6cc5529
Mayank Joshi <emb-mayajos>
2021-01-09 11:18:50 +0530

    Added BookMyLoading URL

--- lib/app_level/configuration.dart
+++ lib/app_level/configuration.dart
@@ -2,4 +2,5 @@
 final String baseRestUrl = "http://192.168.0.193:8080//bookmyloading/api";
 final String baseRestUrlProduction =
     "http://192.168.0.150:8080//bookmyloading/api";
+// final String baseRestUrlProduction = "http://bookmyloading.com/api";
 final int clientId = 1;
